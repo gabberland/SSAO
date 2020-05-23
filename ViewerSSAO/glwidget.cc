c60f9322617add1e6059151954891b72e319efec
gabberland <oriol.giralt@yahoo.es>
2020-05-23 11:59:02 +0200

    Add files via upload

--- ViewerSSAO/glwidget.cc
+++ ViewerSSAO/glwidget.cc
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <random>
 
 #include "./mesh_io.h"
 #include "./triangle_mesh.h"
@@ -20,13 +19,8 @@ const double kZFar = 10;
 const char kLightVertexShaderFile[] = "../shaders/light.vert";
 const char kLightFragmentShaderFile[] = "../shaders/light.frag";
 
-const char kSSAOVertexShaderFile[] = "../shaders/ssao.vert";
-const char kSSAOFragmentShaderFile[] = "../shaders/ssao.frag";
-
 const int kVertexAttributeIdx = 0;
 const int kNormalAttributeIdx = 1;
-
-std::vector<Eigen::Vector3f>  kSSAOKernel;
 
 bool ReadFile(const std::string filename, std::string *shader_source) {
   std::ifstream infile(filename.c_str());
@@ -90,13 +84,7 @@ bool GLWidget::LoadModel(const QString &filename) {
     camera_.UpdateModel(mesh_->min_, mesh_->max_);
 
     // TODO(students): Create / Initialize buffers.
-    mesh_->setUpMesh();
 
-    setupGBuffer();
-
-    setupSSAO();
-
-    setupNoise();
     // END.
 
     emit SetFaces(QString(std::to_string(mesh_->faces_.size() / 3).c_str()));
@@ -123,21 +111,7 @@ void GLWidget::initializeGL() {
 
   if (!res) exit(0);
 
-  //initialize ssao program
-  ssao_program_ = std::make_unique<QOpenGLShaderProgram>();
-
-  res = LoadProgram(kSSAOVertexShaderFile, kSSAOFragmentShaderFile,
-                         ssao_program_.get());
-  if (!res) exit(0);
-
   initialized_ = true;
-
-  glEnable(GL_NORMALIZE);
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_BACK);
-  glEnable(GL_DEPTH_TEST);
-
-  ssaoKernel_ = std::vector<Eigen::Vector3f>();
 }
 
 void GLWidget::resizeGL(int w, int h) {
@@ -200,8 +174,6 @@ void GLWidget::keyPressEvent(QKeyEvent *event) {
 }
 
 void GLWidget::paintGL() {
-
-  glViewport(0,0,width_,height_);
   glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -223,7 +195,6 @@ void GLWidget::paintGL() {
       GLint projection_location, view_location, model_location,
           normal_matrix_location;
 
-      // The first pass will be the geometry rendering
       light_program_->bind();
       projection_location = light_program_->uniformLocation("projection");
       view_location = light_program_->uniformLocation("view");
@@ -235,219 +206,11 @@ void GLWidget::paintGL() {
       glUniformMatrix4fv(model_location, 1, GL_FALSE, model.data());
       glUniformMatrix3fv(normal_matrix_location, 1, GL_FALSE, normal.data());
 
-      glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
-
       // TODO(students): Implement model rendering.
       glBindVertexArray(mesh_->VAO);
       glDrawElements(GL_TRIANGLES, mesh_->faces_.size(), GL_UNSIGNED_INT, 0);
       glBindVertexArray(0);
-
-      glBindBuffer(GL_ARRAY_BUFFER,0);
-      glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
-
-      glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-      glBindTexture(GL_TEXTURE_2D, gNormal);
-      glGenerateMipmap(GL_TEXTURE_2D);
-
-      glBindTexture(GL_TEXTURE_2D, gPosition);
-      glGenerateMipmap(GL_TEXTURE_2D);
-
-      glBindTexture(GL_TEXTURE_2D, 0);
-
-      glUseProgram(0);
-
-      //The  second Pass is the SSAO
-      camera_.SetViewport();
-      glClearColor(1.0f,1.0f,1.0f,1.0f);
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-      //glBindFramebuffer(GL_FRAMEBUFFER, ssaoFBO);
-
-      ssao_program_->bind();
-
-      glUniformMatrix4fv(ssao_program_->uniformLocation("projection"), 1, GL_FALSE, projection.data());
-
-      glActiveTexture(GL_TEXTURE0);
-      glBindTexture(GL_TEXTURE_2D, gNormal);
-      glUniform1i(ssao_program_->uniformLocation("gNormal"), 0);
-
-      glActiveTexture(GL_TEXTURE1);
-      glBindTexture(GL_TEXTURE_2D, gPosition);
-      glUniform1i(ssao_program_->uniformLocation("gPosition"), 1);
-
-      glActiveTexture(GL_TEXTURE2);
-      glBindTexture(GL_TEXTURE_2D, noiseTexture);
-      glUniform1i(ssao_program_->uniformLocation("texNoise"), 2);
-
-      double width = camera_.GetWidth();
-      double height = camera_.GetHeight();
-
-      glUniform1f(ssao_program_->uniformLocation("width"), width);
-      glUniform1f(ssao_program_->uniformLocation("height"),height);
-
-      glUniform1i(ssao_program_->uniformLocation("radius"), radius_);
-
-      glUniform3fv(ssao_program_->uniformLocation("gPosition"), 64, &ssaoKernel_[0][0]);
-
-      glUniform1i(ssao_program_->uniformLocation("sampleN"),sampleN_);
-
-      paintQuad();
-
-//      glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-      glBindTexture(GL_TEXTURE_2D, ssaoTexture);
-      glGenerateMipmap(GL_TEXTURE_2D);
-      glBindTexture(GL_TEXTURE_2D, 0);
-
-      glUseProgram(0);
-
       // END.
     }
   }
-}
-
-void GLWidget::setupGBuffer()
-{
-    //Generate & Bind GBuffer
-    glGenFramebuffers(1, &gBuffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
-
-    //GNormal
-    glGenTextures(1, &gNormal);
-    glBindTexture(GL_TEXTURE_2D, gNormal);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, width_, height_, 0, GL_RGB, GL_FLOAT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gNormal, 0);
-
-    //Position
-    glGenTextures(1,&gPosition);
-    glBindTexture(GL_TEXTURE_2D, gPosition);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, width_, height_, 0, GL_RGB, GL_FLOAT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gPosition, 0);
-
-    GLuint attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
-    glDrawBuffers(2, attachments);
-
-    //DEPTH BUFFER
-    glGenRenderbuffers(1, &depthrenderbuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width_, height_);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-    //CHECK STATUS
-    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
-
-    //Unbind gBuffer
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-void GLWidget::setupSSAO()
-{
-    camera_.SetViewport();
-    glGenFramebuffers(1, &ssaoFBO);
-    glBindFramebuffer(GL_FRAMEBUFFER, ssaoFBO);
-
-
-    // SSAO texture
-    glGenTextures(1, &ssaoTexture);
-    glBindTexture(GL_TEXTURE_2D, ssaoTexture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F, width_, height_, 0, GL_RGB, GL_FLOAT, NULL);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, ssaoTexture, 0);
-
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        std::cout << "SSAO Framebuffer not complete!" << std::endl;
-
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-}
-
-void GLWidget::setupNoise()
-{
-    //Create the emisphere sample selector ( in tangent space)
-    std::uniform_real_distribution<GLfloat> randomFloats(0.0, 1.0); // generates random floats between 0.0 and 1.0
-    std::default_random_engine generator;
-
-    for (unsigned int i = 0; i < 64; ++i)
-    {
-        Eigen::Vector3f sample(
-                    randomFloats(generator) * 2.0 - 1.,
-                    randomFloats(generator) * 2.0 - 1.0,
-                    randomFloats(generator));
-        sample.normalize();
-        sample *= randomFloats(generator);
-        float scale = float(i) / 64.0;
-        scale = 0.1f + (scale*scale) * (1.0f - 0.1f);
-        sample *= scale;
-        ssaoKernel_.push_back(sample);
-    }
-
-    //Adding randomness(noise) in rotation to the sample selection
-    //in order to do so, we create a small texture of random rotations
-    std::vector<Eigen::Vector3f> ssaoNoise;
-    for (unsigned int i = 0; i < 16; i++)
-    {
-        Eigen::Vector3f noise(
-            randomFloats(generator) * 2.0 - 1.0,
-            randomFloats(generator) * 2.0 - 1.0,
-            0.0f);
-        ssaoNoise.push_back(noise);
-    }
-
-    camera_.SetViewport();
-
-    glGenTextures(1, &noiseTexture);
-    glBindTexture(GL_TEXTURE_2D, noiseTexture);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, 4, 4, 0, GL_RGB, GL_FLOAT, &ssaoNoise[0]);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-}
-
-void GLWidget::paintQuad()
-{
-    //inizialization
-    if (quadVBO == 0){
-        std::cout << "Defining quad..." << std::endl;
-
-        GLfloat quadVertices[] =
-        {
-             // Positions
-            -1.0f,  1.0f, -0.05f,
-            -1.0f, -1.0f, -0.05f,
-             1.0f,  1.0f, -0.05f,
-             1.0f, -1.0f, -0.05f,
-        };
-
-        GLuint quadIndices[] = {
-            0, 1, 3,
-            1, 2, 3
-        };
-
-        glBindVertexArray(quadVAO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices)*sizeof(float), quadVertices, GL_STATIC_DRAW);
-
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices)*sizeof(int), quadIndices, GL_STATIC_DRAW);
-
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
-    }
-        glBindVertexArray(quadVAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
-
 }
